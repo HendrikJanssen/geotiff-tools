5b8e0127a2c2991aadd48fa077944d6c1687ed59
Hendrik Janßen <kontakt@hendrik-janssen.de>
2022-05-22 01:29:23 +0200

    feat: Implement documentation site

---
+++ deploy-documentation.sh
@@ -0,0 +1,18 @@
+# Load .env if it exists
+if [ -f .env ]; then
+  export $(echo $(cat .env | sed 's/#.*//g'| xargs) | envsubst)
+fi
+
+# Update javadoc
+./gradlew javadoc
+rsync -avu --delete ./build/docs/javadoc/ ./documentation/public/javadoc
+
+# Build documentation
+cd ./documentation || exit
+npm run build
+
+# Upload to S3 bucket and invalidate cloudfront distribution
+cd ./dist || exit
+aws s3 sync . s3://"$S3_BUCKET_NAME"
+aws cloudfront create-invalidation --distribution-id "$CLOUDFRONT_DISTRIBUTION_ID" --paths "/*"
+
